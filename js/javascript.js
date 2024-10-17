ab480bc9a3fd2d45b977f647fdfde766f1c6a314
makaylacrenshaw03 <134019261+makaylacrenshaw03@users.noreply.github.com>
2024-10-17 16:26:07 -0700

    layout updates

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,8 @@
-$(document).ready(function() {
-  $('.experiment-image-1').click(function() {
-    $('.experiment-image-2').fadeToggle("slow");
+$(document).ready(function () {
+  $(".experiment-image-1").click(function () {
+    $(".experiment-image-2").fadeToggle("slow");
     // Alternative animation for example
     // slideToggle("fast");
   });
 });
+// slideToggle("fast");
